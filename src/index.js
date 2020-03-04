7440be9bd1eac85425c7d37585164595060d8d19
Евгений Шныркевич <mogrein.dark@gmail.com>
2020-03-04 12:52:29 +0300

    feat: realize morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let mas = [];
+    let decStr = "";
+
+    for(let i = 0, j = 0; i < expr.length; i+=10, j++){
+        mas[j] = expr.slice(i, i+10).replace(/00/g, ' ').replace(/10/g, '.').replace(/11/g, '-');
+    }
+
+    for(let i = 0; i < mas.length; i++){
+        let subst = "";
+        for(let j = 0; j < mas[i].length; j++){
+            if(mas[i][j] != ' '){
+                subst += mas[i][j];
+            }
+        }
+        mas[i] = subst;
+        if(mas[i] == "**********"){
+            decStr += " ";
+        } else {
+            decStr += MORSE_TABLE[mas[i]];
+        }
+    }
+    
+    return decStr;
 }
 
 module.exports = {
